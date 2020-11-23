e61f9f81e05860113d901b7826435d9ab12d4c7e
IlyasIt <ilyas.musin.172001@mail.ru>
2020-11-23 17:32:16 +0300

    Updates

---
+++ precache-manifest.cb33d543fd91c9adf989165e5892d9b4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdab6a663d47fee680aa59693a6c630",
+    "url": "/good_vs_evil/index.html"
+  },
+  {
+    "revision": "73efab1ec335a278c03b",
+    "url": "/good_vs_evil/static/css/2.8cdfe4cf.chunk.css"
+  },
+  {
+    "revision": "86433a5579995b5e18d1",
+    "url": "/good_vs_evil/static/css/main.86b84db7.chunk.css"
+  },
+  {
+    "revision": "73efab1ec335a278c03b",
+    "url": "/good_vs_evil/static/js/2.aaca6752.chunk.js"
+  },
+  {
+    "revision": "4150a627f5138970747231fcef63f425",
+    "url": "/good_vs_evil/static/js/2.aaca6752.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86433a5579995b5e18d1",
+    "url": "/good_vs_evil/static/js/main.8480f447.chunk.js"
+  },
+  {
+    "revision": "e79780b47ee0d547686f",
+    "url": "/good_vs_evil/static/js/runtime-main.fda217f5.js"
+  },
+  {
+    "revision": "8eb9fd93dd42e4c25d4ed3aa408c3c81",
+    "url": "/good_vs_evil/static/media/VS.8eb9fd93.png"
+  },
+  {
+    "revision": "e36aaf815eb46c6c0d45fe7d1e3a0476",
+    "url": "/good_vs_evil/static/media/clicking.e36aaf81.png"
+  },
+  {
+    "revision": "e4bd95a50a73c3a502db907058b00507",
+    "url": "/good_vs_evil/static/media/icon.e4bd95a5.png"
+  }
+]);
